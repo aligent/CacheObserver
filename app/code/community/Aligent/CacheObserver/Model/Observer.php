42d9cacdc5a6cee05f4ea27f1c8cb9ca67cc1cb4
Jim O'Halloran <jim@aligent.com.au>
2012-02-23 10:04:45 +1030

    Allowed the user to enable or disable caching of certain blocks vis System > Configuration.

--- app/code/community/Aligent/CacheObserver/Model/Observer.php
+++ app/code/community/Aligent/CacheObserver/Model/Observer.php
@@ -8,6 +8,12 @@ class Aligent_CacheObserver_Model_Observer{
     // You can make this to be configurable at Admin Panel
     const CUSTOM_CACHE_LIFETIME = 3600;
     
+    const ENABLE_CMS_BLOCKS = 'system/cacheobserver/enable_cms_blocks';
+    const ENABLE_CMS_PAGES = 'system/cacheobserver/enable_cms_pages';
+    const ENABLE_CATEGORY_VIEW = 'system/cacheobserver/enable_category_view';
+    const ENABLE_PRODUCT_VIEW = 'system/cacheobserver/enable_product_view';
+    
+    
     // The non-CMS Block you want to cache
     private $cacheableBlocks = array();
 
@@ -16,28 +22,32 @@ class Aligent_CacheObserver_Model_Observer{
             $event = $observer->getEvent();
             $block = $event->getBlock();
             $class = get_class($block);
-            if (('Mage_Cms_Block_Block' == $class) && $block->getBlockId()) {
+            if (('Mage_Cms_Block_Block' == $class) && $block->getBlockId() && Mage::getStoreConfig(self::ENABLE_CMS_BLOCKS)) {
                 $block->setData('cache_lifetime', self::CUSTOM_CACHE_LIFETIME);
                 $block->setData('cache_key', 'cms_block_' . $block->getBlockId() . '_store_' . Mage::app()->getStore()->getId());
                 $block->setData('cache_tags', array(Mage_Core_Model_Store::CACHE_TAG, $block->getBlockId()));
-            } elseif (('Mage_Cms_Block_Page' == $class) && $block->getPage()->getIdentifier()) {
+                
+            } elseif (('Mage_Cms_Block_Page' == $class) && $block->getPage()->getIdentifier() && Mage::getStoreConfig(self::ENABLE_CMS_PAGES)) {
                 $block->setData('cache_lifetime', self::CUSTOM_CACHE_LIFETIME);
                 $block->setData('cache_key', 'cms_page_' . $block->getPage()->getIdentifier() . '_store_' . Mage::app()->getStore()->getId());
                 $block->setData('cache_tags', array(Mage_Core_Model_Store::CACHE_TAG,
                                $block->getPage()->getIdentifier()));
-            } elseif (('Mage_Catalog_Block_Product_View' == $class)) {
+            
+            } elseif (('Mage_Catalog_Block_Product_View' == $class && Mage::getStoreConfig(self::ENABLE_PRODUCT_VIEW))) {
                 $oProduct = Mage::registry('product');
                 $vAlias = $block->getNameInLayout();
                 $block->setData('cache_lifetime', self::CUSTOM_CACHE_LIFETIME);
-                $block->setData('cache_key', 'product_page_' . $oProduct->getId().(Mage::getSingleton('customer/session')->isLoggedIn() ? '_loggedin' : '_loggedout') . '_store_' . Mage::app()->getStore()->getId().'_'.$vAlias);
+                $block->setData('cache_key', 'catalog_product_page_' . $oProduct->getId().(Mage::getSingleton('customer/session')->isLoggedIn() ? '_loggedin' : '_loggedout') . '_store_' . Mage::app()->getStore()->getId().'_'.$vAlias);
                 $block->setData('cache_tags', array(Mage_Core_Model_Store::CACHE_TAG,
                                 $oProduct->getId()));
-            } elseif (('Mage_Catalog_Block_Category_View' == $class)) {
+                
+            } elseif (('Mage_Catalog_Block_Category_View' == $class && Mage::getStoreConfig(self::ENABLE_CATEGORY_VIEW))) {
                 $sCachekey = $this->_generateCategoryCacheKey($observer);
                 $block->setData('cache_lifetime', self::CUSTOM_CACHE_LIFETIME);
-                $block->setData('cache_key', 'product_page_' . $sCachekey);
+                $block->setData('cache_key', 'catalog_category_page_' . $sCachekey);
                 $block->setData('cache_tags', array(Mage_Core_Model_Store::CACHE_TAG,
                                 $sCachekey));
+                
             } elseif (in_array($class, $this->cacheableBlocks)) {
                 $block->setData('cache_lifetime', self::CUSTOM_CACHE_LIFETIME);
                 $block->setData('cache_key', 'block_' . $class . '_store_' . Mage::app()->getStore()->getId());
